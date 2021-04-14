5ea41ae9ec4a9643badef1de06279014002a3678
murilosilverio78 <murilo.silverio@hotmail.com>
2021-04-13 21:27:10 -0300

    Alteraçoes Murilo

--- backend/controllers/LegendariesController.js
+++ backend/controllers/LegendariesController.js
@@ -7,6 +7,58 @@ const controller = {
         const legendary = LegendariesService.listPokemonData(name);        
 
         return res.json(legendary);
+    },
+    create: (req, res) => {
+        const { 
+            name, 
+            description, 
+            type, 
+            healthPoints, 
+            specialAttack, 
+            defense, 
+            attack, 
+            experience, 
+            specialDefense
+        } = req.body;
+        
+        if (!description) {
+            return res.status(400).json({error: "Description is required"});
+        }
+        
+        const legendary = LegendariesService.createLegendary(
+            name, 
+            description, 
+            type, 
+            healthPoints, 
+            specialAttack, 
+            defense, 
+            attack, 
+            experience, 
+            specialDefense
+        );
+
+        return res.json(legendary);
+    },
+    update: (req,res) => {
+        const { id } = req.params;
+        const { 
+            name, 
+            description, 
+            type, 
+            healthPoints, 
+            specialAttack, 
+            defense, 
+            attack, 
+            experience, 
+            specialDefense
+        } = req.body;
+
+        if (!description) {
+            return res.status(400).json({error: "Description is required"});
+        }
+
+        res.json();
+
     }
 }
 
