665c6fe4c721158a160e1ddef7fbb0382dc95d15
murilosilverio78 <murilo.silverio@hotmail.com>
2021-04-14 21:27:56 -0300

    murilo

---
+++ backend/controllers/FileController.js
@@ -0,0 +1,12 @@
+const controller = {
+    storeFile: (req, res) => {
+        const { filename } = req.file;
+        console.log(req.file);
+
+        return res.json({
+            url: 'http://localhost:3333/uploads/${filename}'
+        });
+    }
+}
+
+module.exports = controller
